013a79fe01bda787dec89c4aa4fdaab062220d88
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2025-03-25 08:11:14 -0300

    Enviando tudo.

---
+++ genres/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from genres.models import Genre
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+class GenreSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Genre
+        fields = '__all__'
