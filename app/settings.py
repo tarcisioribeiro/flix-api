a716407b84a1015f6987eb1c3ac77f6fbf277ada
tarcisioribeiro <tarcisio.ribeiro.1840@hotmail.com>
2025-04-09 16:42:46 -0300

    Corrigindo a instalação.

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lhyb6ilk@8c^=r!)aw*srkjlj7z1b&%7pgv%a7mq6hsz4dgw0k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
